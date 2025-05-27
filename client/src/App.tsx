60a92952836492f2cd53b72f4bc865d2fef27c57
Aniruddh Ballal <131568375+aniruddhballal@users.noreply.github.com>
2025-05-27 17:05:46 +0530

    Update App.tsx
    adding custom toast and confirm modules(?)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 function App() {
   return (
@@ -12,6 +15,15 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <ToastContainer 
+        position="top-right"
+        autoClose={1500} // closes automatically in 1.5 second
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
     </BrowserRouter>
   );
 }
