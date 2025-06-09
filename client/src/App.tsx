d19c984386c73e4e0c251ada8c67575d7a5e31b9
Aniruddh Ballal <131568375+aniruddhballal@users.noreply.github.com>
2025-06-09 15:18:05 +0530

    auth middleware logic modified

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,81 +7,92 @@ import Dashboard from "./pages/Dashboard";
 import ProductDisplay from "./pages/ProductDisplay";
 import UserDisplay from "./pages/UserDisplay";
 import EditProfile from "./pages/EditProfile";
+import AuthProvider from "./components/AuthProvider";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import { store } from "./store";
-
 import "react-toastify/dist/ReactToastify.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
 // Protected route logic
 function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const { user, isInitialized, isLoading } = useSelector((state: RootState) => state.auth);
+  
+  if (!isInitialized || isLoading) {
+    return <div>Loading...</div>; // Show loading while checking auth
+  }
+  
   if (!user) {
     return <Navigate to="/login" replace />;
   }
+  
   return children;
 }
 
 // Redirect "/" based on login status
 function HomeRedirect() {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const { user, isInitialized, isLoading } = useSelector((state: RootState) => state.auth);
+  
+  if (!isInitialized || isLoading) {
+    return <div>Loading...</div>; // Show loading while checking auth
+  }
+  
   return <Navigate to={user ? "/dashboard" : "/login"} replace />;
 }
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeRedirect />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeRedirect />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/product/:id"
+              element={
+                <ProtectedRoute>
+                  <ProductDisplay />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/user/:id"
+              element={
+                <ProtectedRoute>
+                  <UserDisplay />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/edit-profile/:id"
+              element={
+                <ProtectedRoute>
+                  <EditProfile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <ToastContainer
+            position="top-right"
+            autoClose={1500}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            pauseOnHover
+            draggable
           />
-          <Route
-            path="/product/:id"
-            element={
-              <ProtectedRoute>
-                <ProductDisplay />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/user/:id"
-            element={
-              <ProtectedRoute>
-                <UserDisplay />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit-profile/:id"
-            element={
-              <ProtectedRoute>
-                <EditProfile />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-
-        <ToastContainer
-          position="top-right"
-          autoClose={1500}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          pauseOnHover
-          draggable
-        />
-      </BrowserRouter>
+        </BrowserRouter>
+      </AuthProvider>
     </Provider>
   );
 }
