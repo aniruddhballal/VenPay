67bbf69ad243cb869aba5df0659774de9d770ef1
Aniruddh Ballal <131568375+aniruddhballal@users.noreply.github.com>
2025-06-15 13:12:46 +0530

    Update AuthLogo.tsx

--- client/src/components/AuthLogo.tsx
+++ client/src/components/AuthLogo.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
 
 // ===== STYLES SECTION =====
-import { logoContainerStyles, logoImageStyles, textImageStyles} from '../styles/loginStyles'
+import { authLogoStyles } from "../styles/loginStyles";
 
 export const AuthLogo = () => (
-  <Box sx={logoContainerStyles}>
-    <Box component="img" src="/images/venpay-logo.png" sx={logoImageStyles} />
-    <Box component="img" src="/images/venpay-text.png" sx={textImageStyles} />
+  <Box sx={authLogoStyles.logoContainer}>
+    <Box component="img" src="/images/venpay-logo.png" sx={authLogoStyles.logoImage} />
+    <Box component="img" src="/images/venpay-text.png" sx={authLogoStyles.textImage} />
   </Box>
 );
